970e78cd3bf41fc20dab51729e0183600a2b8274
kjmarek <43364226+kjmarek@users.noreply.github.com>
2018-10-05 13:58:40 -0500

    added jquery library

--- javascript/timeline.js
+++ javascript/timeline.js
@@ -1,3 +1,5 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 $(".step").click( function() {
 	$(this).addClass("active").prevAll().addClass("active");
 	$(this).nextAll().removeClass("active");
