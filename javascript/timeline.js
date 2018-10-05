ffc5508b88bee7557fd2ddcbf086e94bcb887261
kjmarek <43364226+kjmarek@users.noreply.github.com>
2018-10-05 14:04:43 -0500

    got rid of jquerylibrary

--- javascript/timeline.js
+++ javascript/timeline.js
@@ -1,5 +1,3 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
 $(".step").click( function() {
 	$(this).addClass("active").prevAll().addClass("active");
 	$(this).nextAll().removeClass("active");
